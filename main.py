0d8e0c1be2bb52d64cf3c0502c61b57cdd4540ce
mdufour <mathieu.dufour@whereyoulove.com>
2020-02-13 12:12:20 +0100

    add gpu slection at start of the program

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ import numpy
 import cv2
 from sklearn.model_selection import train_test_split
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only use the first GPU
+  try:
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
+
 def main(argv):
   """
   Main function to manage commands and what to do.
